ef0adc8f96b12066741ec8c159f5b30902f89b13
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2025-07-28 18:03:00 +0100

    feat(wip): test golgi cell models

--- code/model/cells/Golgi/testcells.py
+++ code/model/cells/Golgi/testcells.py
@@ -10,13 +10,10 @@ Author: Ankur Sinha <sanjay DOT ankur AT gmail DOT com>
 
 import glob
 import logging
-import sys
 
-import neuroml
-from neuroml.utils import component_factory
-from pyneuroml.io import read_neuroml2_file, write_neuroml2_file
-from pyneuroml.lems import generate_lems_file_for_neuroml
-from pyneuroml.runners import run_lems_with_jneuroml_neuron
+import numpy
+from pyneuroml.analysis import generate_current_vs_frequency_curve
+from pyneuroml.io import read_neuroml2_file
 
 logger = logging.getLogger("simple_example")
 logger.setLevel(logging.DEBUG)
@@ -31,56 +28,28 @@ logger.addHandler(ch)
 logger.propagate = False
 
 
-def test_cells(amplitude: str = "0.05nA") -> None:
+def test_cells() -> None:
     """Test cells with input currents"""
-    print(f"Generating network with input: {amplitude}")
-    neuroml_file = "GoC_cells_test.net.nml"
-    cellfiles = glob.glob("*.cell.nml")
+    cellfiles = glob.glob("GoC_0*.cell.nml")
     logger.debug(f"Cell files are: {cellfiles}")
 
-    newdoc = component_factory(neuroml.NeuroMLDocument, id="test_golgi_cells")
-    net = newdoc.add(neuroml.Network, id="test_golgi_cell_network", validate=False)
-
-    pg = newdoc.add(
-        neuroml.PulseGenerator,
-        id="pg",
-        delay="250 ms",
-        duration="1000ms",
-        amplitude=amplitude,
-    )
-
-    ctr = 0
-    for afile in cellfiles:
-        newdoc.add(neuroml.IncludeType, href=afile)
-        acell = read_neuroml2_file(afile).cells[0]
-        pop = net.add(
-            neuroml.Population, id=f"golgi_cell_pop_{ctr}", size=1, component=acell.id
+    # TODO: what current do Golgi cells receive in reality?
+    for cellfile in cellfiles[:1]:
+        cell = read_neuroml2_file(cellfile).cells[0]
+        cell_id = cell.id
+        generate_current_vs_frequency_curve(
+            cellfile,
+            cell_id=cell_id,
+            start_amp_nA=0.0,
+            custom_amps_nA=numpy.arange(-50.0, 10.0, 5.0, dtype=float),
+            pre_zero_pulse=200,
+            analysis_duration=500,
+            plot_voltage_traces=True,
+            num_processors=8,
+            simulator="jNeuroML_NEURON",
+            dt=0.01,
         )
-        exp_input = net.add(
-            neuroml.ExplicitInput, target=f"{pop.id}[{ctr}]", input=pg.id
-        )
-        ctr += 1
-
-    logger.debug(newdoc)
-    write_neuroml2_file(newdoc, neuroml_file)
-
-    quantities, sim = generate_lems_file_for_neuroml(
-        sim_id="test_golgi_cells",
-        neuroml_file=neuroml_file,
-        target=net.id,
-        duration="1500 ms",
-        dt="0.01",
-        lems_file_name="LEMS_test_Golgi_cells.xml",
-        target_dir=".",
-    )
-
-    run_lems_with_jneuroml_neuron(
-        "LEMS_test_Golgi_cells.xml", nogui=True, skip_run=False
-    )
 
 
 if __name__ == "__main__":
-    if len(sys.argv) == 2:
-        test_cells(sys.argv[1])
-    else:
-        test_cells()
+    test_cells()
